d35ea48c35c759502f80ceea15489f784f4db76c
Mohammad Shazny <shaznykd@gmail.com>
2022-03-26 11:41:19 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using TutorProject.DB;
+using TutorProject.Models;
+
+namespace TutorProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext context;
+
+        public HomeController(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            List<StudentModel> data = context.StudentData.ToList();
+            return View(data);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(StudentModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    context.StudentData.Add(model);
+                    int result = context.SaveChanges();
+                    if (result>0)
+                    {
+                        return RedirectToAction(nameof(Index),"Home","Success");
+                    }
+                    else
+                    {
+                        return RedirectToAction(nameof(Create),"Home","UploadError");
+                    }
+                }
+                else
+                {
+                    return RedirectToAction(nameof(Create),Response.StatusCode);
+                }
+            }
+            catch (Exception er)
+            {
+                return RedirectToAction(nameof(Create),"Home",er.Message);
+            }
+        }
+    }
+}
---
+++ Models/StudentModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TutorProject.Models
+{
+    [Table("Tbl_Student")]
+    public class StudentModel
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string NIC { get; set; }
+        [Required(ErrorMessage ="Please provide a valid Address")]
+        [DataType(DataType.MultilineText)]
+        public string Address { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public DateTime DOB { get; set; }
+        [Required]
+        public string Contact { get; set; }
+        [Required]
+        public string Status { get; set; }
+    }
+}
